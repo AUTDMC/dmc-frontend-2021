41b5a220d5cb55734b65908b3bdea91cad7ad1ab
Hadi Tabatabaei <hadi.tabatabaei@divar.ir>
2021-10-13 16:58:31 +0330

    fix

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,11 +1,16 @@
 const HTTP_SERVER_ERROR = 500;
-const HTTP_NOT_FOUND = 400;
+const HTTP_NOT_FOUND = 404;
+const HTTP_BAD_REQUEST = 400;
 const HTTP_PERMANENT_REDIRECT = 301;
 
 export function isServerError(status) {
   return status >= HTTP_SERVER_ERROR;
 }
 
+export function isNotFound(status) {
+  return status === HTTP_NOT_FOUND;
+}
+
 export function isRedirect(status) {
-  return status >= HTTP_PERMANENT_REDIRECT && status < HTTP_NOT_FOUND;
+  return status >= HTTP_PERMANENT_REDIRECT && status < HTTP_BAD_REQUEST;
 }
